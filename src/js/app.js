7d603f400167c391013f504d1427cc93baae3375
xuziang111 <584788096@qq.com>
2018-03-16 17:28:26 +0800

    添加登录

---
+++ src/js/app.js
@@ -0,0 +1,47 @@
+let app= new Vue({
+    el:'#app',
+    data: {
+        editingName: false,
+        resume: {
+            name: 'xxx',
+            gender: '男',
+            birthday: '',
+            jobTitle: '前端工程师',
+            phone: '1111111111111',
+            email: 'example@example.com',
+        },
+    },
+    methods:{
+        onEdit(key,value){
+            this.resume[key] = value;
+        },
+        onClickSave() {
+            let currentUser = AV.User.current();
+            console.log(currentUser)
+            if (!currentUser) {
+                this.showLogin()
+            } else {
+                this.saveResume()
+            }
+
+            // var User = AV.Object.extend('User');
+            // // 新建对象
+            // var user = new User();
+            // // 设置名称
+            // user.set('resume',this.resume);
+            // // 设置优先级
+            // user.set('priority',1);
+            // user.save().then(function (user) {
+            //     console.log('objectId is ' + user.id);
+            // }, function (error) {
+            //     console.error(error);
+            // });
+        },
+        showLogin(){
+
+        },
+        saveResume(){
+
+        },
+    }
+})
---
+++ src/js/av.js
@@ -0,0 +1,7 @@
+let APP_ID = '3LkkOf3GnongPOsQ4JfmD2k7-gzGzoHsz';
+let APP_KEY = 'tACRhksC29jJVuvTuNKJTJUw';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
