4d4cff97ea882a8661954b46a31a0c11a0f55810
xuziang111 <584788096@qq.com>
2018-03-17 14:24:11 +0800

    保存基本信息

--- src/js/app.js
+++ src/js/app.js
@@ -1,91 +1,146 @@
-let app= new Vue({
-    el:'#app',
-    data: {
-        loginVisible:false,
-        editingName: false,
-        signUpVisible:true,
-        resume: {
-            name: 'xxx',
-            gender: '男',
-            birthday: '',
-            jobTitle: '前端工程师',
-            phone: '1111111111111',
-            email: 'example@example.com',
-        },
-        signUp:{
-            email:'',
-            password:'',
-        },
-        login:{
-            email:'',
-            password:'',
-        }
-    },
-    methods:{
-        onEdit(key,value){
-            this.resume[key] = value;
-        },
-        onClickSave() {
-            let currentUser = AV.User.current();
-            console.log(currentUser)
-            if (!currentUser) {
-                this.showLogin()
-                this.loginVisible = true
-            } else {
-                console.log()
-                this.saveResume()
+{
+    let app = new Vue({
+        el: '#app',
+        data: {
+            editingName: false,
+            loginVisible: false,
+            signUpVisible: false,
+            currentUser: {
+                objectId: undefined,
+                email:undefined,
+            },
+            resume: {
+                name: 'xxx',
+                gender: '男',
+                birthday: '',
+                jobTitle: '前端工程师',
+                phone: '1111111111111',
+                email: 'example@example.com',
+            },
+            signUp: {
+                email: '',
+                password: '',
+            },
+            login: {
+                email: '',
+                password: '',
             }
-
-            // var User = AV.Object.extend('User');
-            // // 新建对象
-            // var user = new User();
-            // // 设置名称
-            // user.set('resume',this.resume);
-            // // 设置优先级
-            // user.set('priority',1);
-            // user.save().then(function (user) {
-            //     console.log('objectId is ' + user.id);
-            // }, function (error) {
-            //     console.error(error);
-            // });
         },
-        showLogin(){
-
-        },
-        saveResume(){
-            let {id} = AV.User.current()
-            var todo = AV.Object.createWithoutData('User', id);
-            // 修改属性
-            todo.set('resume', this.resume);
-            // 保存到云端
-            todo.save();
-        },
-        onSignUp(){
-            console.log(this.signUp)
-            const user = new AV.User()
-            user.setUsername(this.signUp.email)
-            user.setPassword(this.signUp.password)
-            user.setEmail(this.signUp.email)
-            user.signUp().then(function (user) {
-                console.log(user);
-            }, function (error) {
-            });
-
-        },
-        onLogin(e){
-            AV.User.logIn(this.login.email, this.login.password).then(function (loginedUser) {
-                console.log(loginedUser);
-            }, function (error) {
-                if(error.code === 211){
-                    alert('邮箱不存在')
-                }else if(error.code === 210){
-                    alert('邮箱密码不匹配')
+        methods: {
+            onEdit(key, value) {
+                this.resume[key] = value;
+            },
+            onClickSave() {
+                let currentUser = AV.User.current();
+                console.log(currentUser)
+                if (!currentUser) {
+                    this.showLogin()
+                    this.loginVisible = true
+                } else {
+                    console.log()
+                    this.saveResume()
                 }
-            });
-        },
-        onLogout(w){
-            AV.User.logOut()
-            alert('注销成功')//清除用户缓存
+
+                // var User = AV.Object.extend('User');
+                // // 新建对象
+                // var user = new User();
+                // // 设置名称
+                // user.set('resume',this.resume);
+                // // 设置优先级
+                // user.set('priority',1);
+                // user.save().then(function (user) {
+                //     console.log('objectId is ' + user.id);
+                // }, function (error) {
+                //     console.error(error);
+                // });
+            },
+            showLogin() {
+
+            },
+            saveResume() {
+                let {objectId} = AV.User.current().toJSON()
+                var user = AV.Object.createWithoutData('User', objectId);
+                // 修改属性
+                user.set('resume', this.resume);
+                // 保存到云端
+                user.save().then(()=>{
+                    alert('保存成功')
+                },()=>{
+                    alert('保存失败')
+                });
+            },
+            onSignUp() {
+                console.log(this.signUp)
+                const user = new AV.User()
+                user.setUsername(this.signUp.email)
+                user.setPassword(this.signUp.password)
+                user.setEmail(this.signUp.email)
+                user.signUp().then( (user) => {
+                    console.log(user)
+                    alert('注册成功')
+                    loginVisible = false
+                    signUpVisible = false
+                    AV.User.logIn(this.signUp.email, this.signUp.password).then( (user) => {
+                        console.log(user);
+                        user = user.toJSON()
+                        this.loginVisible = false
+                        this.currentUser = {
+                            objectId: user.objectId,
+                            email: user.email,//
+                        }
+                    })
+
+
+                },  (error) => {
+                    alert(error.rawMessage)
+                });
+
+            },
+            hasLogin(){
+                return !!this.currentUser.objectId
+            },
+            onLogin(e) {
+                AV.User.logIn(this.login.email, this.login.password).then( (user) => {
+                    console.log(user);
+                    user = user.toJSON()
+                    this.loginVisible = false
+                    this.currentUser = {
+                        objectId: user.objectId,
+                        email: user.email,//
+                    }
+                },  (error) => {
+                    if (error.code === 211) {
+                        alert('邮箱不存在')
+                    } else if (error.code === 210) {
+                        alert('邮箱密码不匹配')
+                    }
+                });
+            },
+            onLogout(w) {
+                AV.User.logOut()
+                this.currentUser = {
+                    objectId: undefined,
+                    email:undefined,
+                }
+                alert('注销成功')//清除用户缓存
+            },
+            getResume(){
+                let query = new AV.Query('User');
+                query.get(this.currentUser.objectId).then((user) => {
+                    // 成功获得实例
+                    console.log(user)
+                    let resume = user.toJSON().resume
+                    this.resume = resume
+                },(error) => {
+                    // 异常处理
+                });
+            }
         }
+    })
+let currentUser = AV.User.current()
+    console.log(currentUser)
+    if(currentUser){
+        app.currentUser = currentUser.toJSON()
+        app.getResume()
     }
-})
+}
